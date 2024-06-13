05ea73afd2b827ca4862967a826c656c349ab6d8
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-06-13 13:45:01 +0200

    finished weather-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [city, SetCity] = useState();
+  const [temp, SetTemp] = useState(0);
+
+  function success(pos) {
+    setLatitude(pos.coords.latitude);
+    setLongitude(pos.coords.longitude);
+  }
+
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  const gotWeather = async () => {
+    try {
+      debugger
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=8988d33759d408415270f13c41a0f52b`
+      );
+      SetCity(response.data.name);
+      SetTemp(response.data.main.temp - 273);
+    } catch (error) {
+      console.log(`error!!!`);
+    }
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }, []);
+
+  useEffect(() => {
+    if (latitude && longitude) {
+      gotWeather();
+    }
+  }, [latitude,longitude]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        latitude: {latitude}, longitude: {longitude}
+      </h1>
+
+      <h1>City {city}</h1>
+      <h1>Temperature:{temp.toFixed(1)}</h1>
     </div>
   );
 }
